05eb7c3ec05e650fd5e1318ffc1bf9875c4e6f39
reza-rezvani2052 <reza-rezvani2052@yahoo.com>
2018-10-06 04:21:59 +0330

    Fonte bakhshe NoInternetActivity ra farsi kardam va be an Animation ezafe kardam

--- app/src/main/java/com/shahraksoft/wemos_relay/NoInternetActivity.java
+++ app/src/main/java/com/shahraksoft/wemos_relay/NoInternetActivity.java
@@ -1,19 +1,37 @@
 package com.shahraksoft.wemos_relay;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.github.hujiaweibujidao.wava.Techniques;
+import com.github.hujiaweibujidao.wava.YoYo;
 
 public class NoInternetActivity extends AppCompatActivity {
+
+    ImageView imgNoInternet;
+    Button btnCheckInternetAgain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_no_internet);
+        //...
+        imgNoInternet = findViewById(R.id.imgNoInternet);
+        btnCheckInternetAgain = findViewById(R.id.btnCheckInternetAgain);
+
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Yekan.ttf");
+        btnCheckInternetAgain.setTypeface(typeface);
+        //btnCheckInternetAgain.setTextSize(16);
+
         //...
 
         //supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -31,6 +49,8 @@ public class NoInternetActivity extends AppCompatActivity {
             startActivity(new Intent(this, MainActivity.class));
             //...
             this.finish();
+        } else {
+            YoYo.with(Techniques.Tada).duration(750).playOn(imgNoInternet);
         }
     }
 
